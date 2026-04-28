b39fdf3573c6c3911dc6ed4ad56406ba2bce037e
chorueang-rose <iamrobinxz@gmail.com>
2026-04-28 21:34:18 +0700

    coin up

--- Assets/Scipts/Coin.cs
+++ Assets/Scipts/Coin.cs
@@ -1,16 +1,26 @@
 using System;
+using TMPro;
 using UnityEngine;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Coin : MonoBehaviour
 {
     public AudioClip coinClip;
+    public int coinToGive = 1;
+    public TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.coins += 1;
+            player.coins += coinToGive;
             player.PlaySFX(coinClip, 0.4f);
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
